bbf8ea58c4a6c95e300f065420363f6fcf428301
Courtland Bourgeois <courtland@CourtlandsMBP2.eatel.net>
2019-12-06 11:29:27 -0600

    initial commit with data displaying from API

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+import Players from './components/Players';
+
+import axios from 'axios';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    players: []
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/api/players")
+      .then(res => {
+        // console.log(res.data)
+        this.setState({
+          players: res.data
+        })
+      })
+      .catch(err => console.log('Why no data?', err))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Women's World Cup</h1>
+        <div>
+          {this.state.players.map(player => (
+            <Players key={player.id} player={player} />
+          ))}
+        </div>
+      </div>
+    ); 
+  }
 }
 
 export default App;
---
+++ client/src/components/Players.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Players = (props) => {
+
+  return (
+    <div>
+      <div className='players'>
+        <h2>{props.player.name}</h2>
+        <h3>{props.player.country}</h3>
+        <h4>Number of Searches: {props.player.searches}</h4>
+        <hr />
+      </div>
+    </div>
+  )
+}
+
+export default Players
